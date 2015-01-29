543dc066c257d3f5678a1caee2fa18d9fa0e2e85
Jamie Patel <jamie@notespublication.com>
2015-01-29 14:47:38 +0000

    Step two

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import pusher
 import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 app_id = os.environ.get('PUSHER_APP_ID')
 key = os.environ.get('PUSHER_APP_KEY')
@@ -11,4 +14,14 @@ p = pusher.Pusher(
   secret=secret
 )
 
-p['my_channel'].trigger('my_event', {'message': 'hello world'})
+@app.route("/")
+def show_index():
+    return render_template('index.html')
+
+@app.route("/notification")
+def trigger_notification():
+	p['notifications'].trigger('new_notification', {'message': 'hello world'})
+	return "Notification triggered!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
